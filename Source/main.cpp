d3f5e770ea8f6336d7d7b5e7322f51799e2b85e3
Frank Müller <frank.mueller@softart.ch>
2017-12-02 21:45:23 +0100

    Fixes bug which caused that wrong time steps were used in motion tracking

--- Source/main.cpp
+++ Source/main.cpp
@@ -11,42 +11,42 @@ int main(int argc, char *argv[])
 	sensor.connect();
 
 	auto tracker = BobOnRails::Firmware::MotionTracker();
-    
-    int sampleTime = 1000000; //The sample time in micro seconds
-    
 
-    timeval now, measurementStartTime, lastMeasurementStartTime;
-    
-    gettimeofday(&lastMeasurementStartTime, NULL);
-    do
-    {
-        gettimeofday(&measurementStartTime, NULL);
+	int sampleTime = 1000000; //The sample time in micro seconds
 
-	auto temperature = 0.0f;
-	auto acceleration = BobOnRails::Firmware::Vector3();
-	auto gyration = BobOnRails::Firmware::Vector3();
-	sensor.measure(&acceleration, &gyration, &temperature);
 
-        printf("Acceleration: (%f, %f, %f)[m/s^2], (%f, %f, %f)[deg/s^2], (%f)[°C]\n", 
-            acceleration.X, acceleration.Y, acceleration.Z,
-            gyration.X, gyration.Y, gyration.Z,
-            temperature);
-    
-        auto timeBetweenMeasurements = 
-            (lastMeasurementStartTime.tv_usec - measurementStartTime.tv_usec) * 1E-6;
-	tracker.appendMotion(timeBetweenMeasurements, acceleration, gyration);
-        
-        gettimeofday(&now, NULL);
-        auto usedCycleTime = now.tv_usec - measurementStartTime.tv_usec;
-    
-        auto currentPosition = tracker.getPosition();
-        printf("Position: (%f, %f, %f)[m]\n", currentPosition);
+	timeval now, lastMeasurementStartTime;
 
-        auto sleepTime = sampleTime - usedCycleTime;    
-        printf("Cycle time was %d micros seconds, sleeping for %d micro seconds\n", usedCycleTime, sleepTime);
-        usleep(sleepTime);
+	gettimeofday(&lastMeasurementStartTime, NULL);
+	do
+	{
+		timeval measurementStartTime;
+		gettimeofday(&measurementStartTime, NULL);
 
-        lastMeasurementStartTime = measurementStartTime;
-    }
-    while (1);
+		auto temperature = 0.0f;
+		auto acceleration = BobOnRails::Firmware::Vector3();
+		auto gyration = BobOnRails::Firmware::Vector3();
+		sensor.measure(&acceleration, &gyration, &temperature);
+
+		printf("Acceleration: (%f, %f, %f)[m/s^2], (%f, %f, %f)[deg/s^2], (%f)[°C]\n",
+			acceleration.X, acceleration.Y, acceleration.Z,
+			gyration.X, gyration.Y, gyration.Z,
+			temperature);
+
+		auto timeBetweenMeasurements =
+			(lastMeasurementStartTime.tv_usec - measurementStartTime.tv_usec) * 1E-6;
+		tracker.appendMotion(timeBetweenMeasurements, acceleration, gyration);
+
+		gettimeofday(&now, NULL);
+		auto usedCycleTime = now.tv_usec - measurementStartTime.tv_usec;
+
+		auto currentPosition = tracker.getPosition();
+		printf("Position: (%f, %f, %f)[m]\n", currentPosition);
+
+		auto sleepTime = sampleTime - usedCycleTime;
+		printf("Cycle time was %d micros seconds, sleeping for %d micro seconds\n", usedCycleTime, sleepTime);
+		usleep(sleepTime);
+
+		lastMeasurementStartTime = measurementStartTime;
+	} while (1);
 }
